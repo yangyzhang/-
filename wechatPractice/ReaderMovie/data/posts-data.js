fc347203c2c887dc652703c1a7859888244db5f0
yangyzhang <yangyzhang16@163.com>
2017-01-01 22:52:28 +0800

    使用本地数据更新页面

---
+++ wechatPractice/ReaderMovie/data/posts-data.js
@@ -0,0 +1,62 @@
+var local_database = [
+  {
+    title: "又是蟹肥虾壮时",
+    content: "菊黄蟹正肥，品尝秋之味。徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
+    imgSrc: "/images/post/crab.png",
+    reading: 62,
+    collection: 92,
+    date: "Sep 20 2016",
+    avatar: "/images/avatar/4.png"
+  },
+  {
+    title: "当我们在谈论经济学时，我们在谈论什么?",
+    content: "引言在我跟学生课后交流时，以及我在知乎上阅读有关“经济”问题的论题时，经常会遇到这样的情况：...",
+    imgSrc: "/images/post/sls.jpg",
+    reading: 62,
+    collection: 92,
+    date: "Nov 12 2016",
+    avatar: "/images/avatar/3.png",
+  },
+  {
+    title: "比利·林恩的中场故事",
+    content: "一 “李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》。",
+    imgSrc: "/images/post/bl.png",
+    reading: 62,
+    collection: 92,
+    date: "Nov 20 2016",
+    avatar: "/images/avatar/1.png",
+  },
+  {
+    title: "微信·小程序开发工具安装指南",
+    content: "这两天闲来无事，也安装了 “微信折叠”的开发工具来玩一下。以下是一些小道消息及使用体验，过两天我会写一篇文章以开发者的角度来详细评价微信小程序",
+    imgSrc: "/images/post/xiaolong.jpg",
+    reading: 102,
+    collection: 92,
+    date: "Nov 20 2016",
+    avatar: "/images/avatar/5.png",
+  },
+  {
+    title: "从视觉到触觉 这款VR手套能给你真实触感",
+    content: "8月29日消息，据国外媒体VentureBeat报道，一家名为Dexta Robotics的公司最近发布了一款有望变革虚拟现实手部追踪与交互方式的新产品",
+    imgSrc: "/images/post/vr.png",
+    reading: 102,
+    collection: 26,
+    date: "Nov 20 2016",
+    avatar: "../../../images/avatar/3.png",
+  },
+  {
+    title: "创维爱奇艺战略合作发布会",
+    content: "9月13日，在京举行的“爱创世纪——爱奇艺&创维战略合作发布会”上，爱奇艺与创维集团联合宣布，双方将联合就客厅娱乐生态展开深度合作",
+    imgSrc: "/images/iqiyi.png",
+    reading: 102,
+    collection: 26,
+    date: "Nov 20 2016",
+    avatar: "/images/avatar/4.png",
+  },
+]
+
+console.log("ddddd")
+
+module.exports = {
+  postList: local_database 
+}
